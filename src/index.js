ed62755aac0aeaa2ec1cc493e47e84382d0c37d3
MrYasuo <buiduchai0706vn@gmail.com>
2021-10-12 11:00:51 +0700

    main

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 mongoose
-	.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test")
+	.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
 	.then(() => console.log("connect successfully"))
 	.catch((err) => console.log(err));
 
@@ -82,5 +85,5 @@ app.post("/change", (req, res) => {
 });
 
 app.listen(port, () => {
-	console.log(`Listening at http://localhost:${port}/group1`);
+	console.log(`Listening at http://localhost:${port}`);
 });
